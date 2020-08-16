9477868852ae76ba45f7f5b43b96d54d0dc8d0de
GishaTheDeveloper <spaceshhz@gmail.com>
2020-08-16 20:42:52 +0300

    Add ShootRay() for Pickaxe

---
+++ Dig and Fight/Assets/Scripts/TilemapEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapEditor : MonoBehaviour
+{
+    #region Singleton
+    public static TilemapEditor Instance { private set; get; }
+    #endregion
+
+    public Tilemap blocksTilemap;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void RemoveTile(Vector2 pos)
+    {
+        Vector3Int intPosition = blocksTilemap.WorldToCell(pos);
+        Debug.Log(intPosition);
+        if (!blocksTilemap.HasTile(intPosition))
+        {
+            Debug.LogError("Tile wasn't found!");
+            return;
+        }
+
+        blocksTilemap.SetTile(intPosition, null);
+    }
+
+
+}
--- Dig and Fight/Assets/Scripts/Tools/Pickaxe.cs
+++ Dig and Fight/Assets/Scripts/Tools/Pickaxe.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
 public class Pickaxe : Tool
 {
+    public float rayLength;
+    public LayerMask collideableLayer;
+
     Animator animator;
 
     void Awake()
     {
-        animator = GetComponent<Animator>();    
+        animator = GetComponent<Animator>();
     }
 
     public override void Interact()
     {
+        ShootRay();
         animator.SetTrigger("Interact");
+    }
+
+    void ShootRay()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, DirToMouse(), rayLength, collideableLayer);
+
+        Debug.DrawRay(transform.position, DirToMouse().normalized * rayLength);
+
+        if (hitInfo.collider != null)
+            if (hitInfo.collider.CompareTag("Block"))
+            {
+                Debug.Log(hitInfo.collider.name);
+
+                TilemapEditor.Instance.RemoveTile(hitInfo.point);
+            }
+                
+    }
+
+    Vector2 DirToMouse()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
     }
 }
