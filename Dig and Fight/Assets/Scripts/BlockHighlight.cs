48f1c4c39ae74601897d6856958b98aacf67194d
GishaTheDeveloper <spaceshhz@gmail.com>
2020-08-17 18:20:56 +0300

    Add BlockHighlight.cs

---
+++ Dig and Fight/Assets/Scripts/BlockHighlight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BlockHighlight : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetHighlight(Vector3 position)
+    {
+        transform.position = position;
+
+        spriteRenderer.enabled = true;
+    }
+
+    public void DisableHighlight()
+    {
+        spriteRenderer.enabled = false;
+    }
+}
