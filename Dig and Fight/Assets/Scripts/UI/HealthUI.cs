4c69da089b35bbfbcc33e4feaa2988d99fe4e199
GishaTheDeveloper <spaceshhz@gmail.com>
2020-08-18 16:44:57 +0300

    Add HealthUI.cs

---
+++ Dig and Fight/Assets/Scripts/UI/HealthUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class HealthUI : MonoBehaviour
+{
+    public GameObject[] healthInsides;
+
+    public void UpdateHealthUI(int h)
+    {
+        for (int i = 0; i < healthInsides.Length; i++)
+        {
+            if (i < h)
+                healthInsides[i].SetActive(true);
+            else
+                healthInsides[i].SetActive(false);
+        }
+    }
+}
