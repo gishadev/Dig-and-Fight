6208e50e8ddf03c1141699f1c2411202fa10dfa8
GishaTheDeveloper <spaceshhz@gmail.com>
2020-08-19 13:42:14 +0300

    Add ScoreSystem.cs

---
+++ Dig and Fight/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    #region Singleton
+    public static ScoreSystem Instance { private set; get; }
+    #endregion
+
+    private int score;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void AddScore(int count)
+    {
+        score += count;
+        UIManager.Instance.scoreUI.UpdateScoreUI(score);
+    }
+}
---
+++ Dig and Fight/Assets/Scripts/UI/ScoreUI.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+   public TMP_Text scoreText;
+
+    public void UpdateScoreUI(int score)
+    {
+        scoreText.text = score.ToString();
+    }
+}
