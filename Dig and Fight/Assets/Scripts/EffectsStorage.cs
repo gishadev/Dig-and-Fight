a5677dfb15bd0b261155939db87930314738352b
GishaTheDeveloper <spaceshhz@gmail.com>
2020-08-19 14:43:46 +0300

    Add EffectsStorage.cs

---
+++ Dig and Fight/Assets/Scripts/EffectsStorage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+
+public class EffectsStorage : MonoBehaviour
+{
+    #region Singleton
+    public static EffectsStorage Instance { private set; get; }
+    #endregion
+
+    public Effect[] effects;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+}
+
+public static class EffectsEmitter
+{
+    public static void Emit(string effectName, Vector2 position)
+    {
+        Effect effect = Array.Find(EffectsStorage.Instance.effects, x => x.name == effectName);
+        GameObject.Instantiate(effect.prefab, position, effect.prefab.transform.rotation);
+    }
+}
+
+[Serializable]
+public class Effect
+{
+    public string name;
+    public GameObject prefab;
+}
